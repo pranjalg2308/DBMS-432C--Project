b529c50ee342535ab74878d33a06b1ab0315d514
Pranjal <pranjalg2308@gmail.com>
2019-04-22 12:20:46 +0530

    Repo Created

---
+++ UrbanClapCloneClient/app/src/main/java/com/kalabhedia/urbanclapclone/MainActivity.java
@@ -0,0 +1,66 @@
+package com.kalabhedia.urbanclapclone;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+    private ActionBar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = getSupportActionBar();
+        setUpNavigationDrawer();
+    }
+
+    private void setUpNavigationDrawer() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (bottomNavigationView.getSelectedItemId() != menuItem.getItemId()) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_bar_home:
+                            navController.navigate(R.id.homeFragment);
+                            navController.popBackStack(R.id.homeFragment, false);
+                            break;
+                        case R.id.nav_bar_bookings:
+                            navController.navigate(R.id.bookingFragment);
+                            break;
+                        case R.id.nav_bar_profile:
+                            navController.navigate(R.id.profileFragment);
+                            break;
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        navController.popBackStack(R.id.homeFragment,false);
+        bottomNavigationView.setSelectedItemId(R.id.nav_bar_home);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(navController, (DrawerLayout) null);
+    }
+}
