5b472c745d13a718d22c7f5f122355ae5bb06602
falconis <panivishal17@gmail.com>
2019-04-29 17:22:39 +0530

    minor changes

--- web/db/make_order.sql
+++ web/db/make_order.sql
@@ -1,4 +1,4 @@
-create or replace procedure make_order (order_id in varchar2, user_id in varchar2, service_id in varchar2, date_of_order date, start_time long, duration number, completed number, deleted number)
+create or replace procedure make_order (order_id in varchar2, user_id in varchar2, service_id in varchar2, date_of_order number, start_time number, duration number, completed number, deleted number)
 is
 begin
     insert into orders(order_id, user_id, service_id, date_of_order, start_time, duration, completed, deleted) values(order_id, user_id, service_id, date_of_order, start_time, duration, completed, deleted);
